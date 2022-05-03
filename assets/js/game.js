1dd234ed1b39740442959c248b0e78aa5c88ef9c
Ryan Murphy <TheDecoder007@gmail.com>
2022-05-03 15:07:39 -0400

    start and end game functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,7 +1,7 @@
 
 var playerName = window.prompt("What is your robots name?");
 var playerHealth = 100;
-var playerAttack = 40;
+var playerAttack = 25;
 var playerMoney = 10;
 
 var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
@@ -34,13 +34,14 @@ var fight = function(enemyName) {
     if (enemyHealth <=0) {
         window.alert(enemyName + " has died!");
         playerMoney = playerMoney + 20;
+        console.log("playerMoney", playerMoney);
         break;
     } else {
         window.alert(enemyName + " still has " + enemyHealth + " health left.");
     }
     
     playerHealth = playerHealth - enemyAttack;
-    console.log(playerHealth);
+    console.log("You have " + playerHealth + " left.");
 
     if (playerHealth <=0) {
         window.alert(playerName + " has died!");
@@ -48,13 +49,18 @@ var fight = function(enemyName) {
     } else {
         window.alert(playerName + " still has " + playerHealth + " health left.");   
     }  
-    //  else {
-    //   window.alert("You need to chose a valid option. Try again.");
-    //   //fight();
-    //  }  
     } 
-
 };
+
+//  else {
+//   window.alert("You need to chose a valid option. Try again.");
+//   //fight();
+//  }  
+    var startGame = function() {
+        playerHealth = 100;
+        playerAttack = 25;
+        playerMoney = 10;
+
 for (var i = 0; i < enemyNames.length; i++) {
     if (playerHealth > 0) {
      window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
@@ -64,6 +70,25 @@ for (var i = 0; i < enemyNames.length; i++) {
     }
     else {
         window.alert("You have lost your robot in battle. Game over!");
+        break;
     }
 }
-//fight();
+    endGame();
+};
+var endGame = function() {
+    if (playerHealth > 0) {
+    window.alert("Great Job, you survived! Your score is " + playerMoney + ".");
+    }
+    else {
+        window.alert("You lost your robot in battle!");
+    }
+var playAgainConfirm = window.confirm("Do you want to play again?");
+    if (playAgainConfirm) {
+        startGame();
+    }
+    else { 
+        window.alert("Thank you for playing!");
+    }
+};
+
+startGame();
